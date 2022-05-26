3f90bb391cb1228d7a95d23586ff46cfad8f53e3
Thomas Copt-Bignon <lowx.add@gmail.com>
2022-05-26 20:15:25 +0200

    Update wiki_relations.py

--- src/wiki_relations.py
+++ src/wiki_relations.py
@@ -1,38 +1,69 @@
-import wikipediaapi as wikiapi
+import requests
+import re
 
 # Params
-seed = 'Oscar Isaac'
-langage = 'fr'
-
-wiki = wikiapi.Wikipedia(langage)
-p = wiki.page(seed)
+seed = 'Albert Einstein'
+langage = 'en'
 
 # Header
 print('WikiRelations.')
-print(f'Starting from {seed} ({p.fullurl})\n')
+print(f'Starting from {seed} :')
+
+# Setup requests
+s = requests.Session()
+url = f"https://{langage}.wikipedia.org/w/api.php"
 
 # Get links from a wiki page
-def get_links(page):
+def get_links(title):
+    
+    # Call the API to retrieve all the links in the page
+    params = {
+        "action": "query",
+        "format": "json",
+        "titles": title,
+        "prop": "links",
+        "pllimit": "max"
+    }
+    response = s.get(url=url, params=params)
+    data = response.json()
+    pages = data["query"]["pages"]
+    links = []
 
-    print(f'Number of links : {len(page.links)}')
+    # Parse the response and return empty array if no page found
+    try:
+        for key, val in pages.items():
+            for link in val["links"]:
+                links.append(link["title"])
+    except KeyError:
+        return []
+    
+    # Keep calling the API if links are missing
+    while "continue" in data:
+        plcontinue = data["continue"]["plcontinue"]
+        params["plcontinue"] = plcontinue
 
-    for l in page.links:
+        response = s.get(url=url, params=params)
+        data = response.json()
+        pages = data["query"]["pages"]
 
-        # Remove links outside
-        if ':' in l:
-            continue
-        
-        # Remove links with number
-        if any(char.isdigit() for char in l):
-            continue
-        
-        # Get page (Cost too much...)
-        pg = wiki.page(l)
+        for key, val in pages.items():
+            for link in val["links"]:
+                links.append(link["title"])
 
-        # Print it title and link
-        try:
-            print("{: <50} {: <50}".format(*[l, pg.fullurl]))
-        except KeyError:
-            continue
+    # Remove links that are are in the footer of the page
+    footer_nb = 0
+    for i in reversed(links):
+        if  ':' in i:
+            footer_nb += 1
+        else:
+            break
+    links = links[:len(links) - footer_nb]
+    
+    # Remove links that ends by a number to remove dates
+    regex = re.compile(r'[0-9]*$')
+    links = [i for i in links if not regex.match(i)]
 
-get_links(p)
+    return links
+
+for i in get_links(seed):
+    print(i)
