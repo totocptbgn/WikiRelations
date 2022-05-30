908d2a25cc9c98a07b8f3bae236952780e66e235
Thomas Copt-Bignon <lowx.add@gmail.com>
2022-05-30 15:28:06 +0200

    Better plot

--- src/plot_graph_wiki.py
+++ src/plot_graph_wiki.py
@@ -8,7 +8,7 @@ filename = 'src/csv/lotr_2.csv'
 # Creating the graph and the dataframe
 g = nx.Graph()
 df = pd.read_csv(filename, sep=';', header=None)
-df = df.sample(frac=0.3)
+df = df.sample(frac=0.01)
 print(f'Dataframe created.')
 
 # print(df.shape)
@@ -27,12 +27,12 @@ print('Layout edges.')
 # Plot and save the ouput
 options = {
     'with_labels': False,
-    'node_color': 'black',
+    'node_color': 'white',
     'node_size': 20,
     'width': 0.5,
     'alpha': 0.5
 }
-fig = plt.figure(1, figsize=(100, 100))
+fig = plt.figure(1, figsize=(40, 40), dpi=300.0, facecolor='black', frameon="False")
 nx.draw_networkx(g, pos, **options)
 print('Drawn graph.')
 plt.savefig("graph.png", format="PNG")
